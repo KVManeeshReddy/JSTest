ad223ec071561cd2f085c5b0dd5c52ba4a093d9d
MartinManeesh <martinmaneesh123@gmail.com>
2019-06-06 12:00:10 +0530

    commit

--- JS/custom.js
+++ JS/custom.js
@@ -1,39 +1,62 @@
+var currentTimer;
+var stopWatch;
+var h = 0;
+var m = 0;
+var s = 0;
+
 const timeFormatter = (i) => {
     if (i < 10) { i = "0" + i }; // add zero in front of numbers < 10
     return i;
 }
 
-const workingClock = () => {
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+const workingClock = (hours, minutes, seconds) => {
+    hours = timeFormatter(hours);
     minutes = timeFormatter(minutes);
     seconds = timeFormatter(seconds);
     document.getElementById("displayClock").innerHTML = `${hours}:${minutes}:${seconds}`;
-}
-var t = setTimeout(workingClock, 500);
-const currentTime = () => {
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    console.log("hi");
-    document.getElementById("displayClock").innerHTML = `${hours}:${minutes}:${seconds}`;
+    currentTimer = setTimeout(workingClockAction, 1000);
 }
 
-const resetClock = () => {
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    minutes = checkTime(minutes);
-    seconds = checkTime(seconds);
-    document.getElementById("displayClock").innerHTML = `${hours}:${minutes}:${seconds}`;
-    clearTimeout(t);
 
-    function checkTime(i) {
-        if (i < 10) { i = "0" + i }; // add zero in front of numbers < 10
-        return i;
+const resetClockAction = () => {
+    clearInterval(currentTimer);
+    timerAction();
+}
+
+const timerAction = () => {
+    timer(h, m, s);
+    s++;
+    if (s == 60) {
+        s = 0;
+        m++;
     }
+    if (m == 60) {
+        m = 0;
+        h++;
+    }
+    if (h == 24) {
+        h = 0;
+    }
+    stopWatch = setTimeout(timerAction, 1000);
 }
-var t1 = setTimeout(resetClock, 500);
+
+const currentTimeAction = () => {
+    clearInterval(stopWatch);
+    h = 0;
+    m = 0;
+    s = 0;
+    let date = new Date();
+    workingClock(date.getHours(), date.getMinutes(), date.getSeconds());
+}
+
+const workingClockAction = () => {
+    let date = new Date();
+    workingClock(date.getHours(), date.getMinutes(), date.getSeconds());
+}
+
+const timer = (h, m, s) => {
+    h = timeFormatter(h);
+    m = timeFormatter(m);
+    s = timeFormatter(s);
+    document.getElementById("displayClock").innerHTML = `${h}:${m}:${s}`;
+}
